3fb6e770c12eadf54aed3d1d47496283d7eaf98d
Ishara Dilshan <isharadilshandev@gmail.com>
2023-04-19 11:36:16 +0530

    minor fixes

--- src/services/rest-client/index.ts
+++ src/services/rest-client/index.ts
@@ -2,10 +2,9 @@ import axios from 'axios';
 
 const headerConfigs = {
   'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*',
 };
 
 export const Axios = axios.create({
-  baseURL: 'http://143.198.206.156:3001',
+  baseURL: 'https://143.198.206.156:3001',
   headers: headerConfigs,
 });
